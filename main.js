2d06cc43f0f099447aa6d473051b49a35f243b58
wanyujun <superinc@qq.com>
2023-09-18 14:27:58 +0800

    Update main.js

--- main.js
+++ main.js
@@ -5,6 +5,7 @@
 // @description  青书自动刷视频脚本，实现自动播放、自动切换章节、自动跳转下一小节。
 // @author       wanyujun
 // @match        https://degree.qingshuxuetang.com/gd/Student/Course/CourseShow*
+// @match        https://degree.qingshuxuetang.com/gd/Student/Course/CourseStudy*
 // @icon         https://degree.qingshuxuetang.com/resources/default/images/favicon.ico
 // @grant        GM_notification
 // @grant        GM_setValue
@@ -18,66 +19,139 @@
 
 (function() {
     'use strict';
-    setTimeout(() => {
-        let el = $('video')
-        if (el.get(0) && el.attr('src')) {
-            let player = el.get(0)
-            GM_setValue(utils.getTryKey(), 5)
-            utils.notify("找到视频组件了，准备开始播放视频")
-            player.addEventListener("ended", function() {
-                utils.notify("视频播放完毕，即将跳转到下一小节")
-                //生成下一节视频NodeId
-                let nodeId = utils.getUrlParam('nodeId')
-                let nextUrl = utils.getNextUrl(nodeId)
-                setTimeout(() => { window.location.href = nextUrl }, 5000)
-            })
-            player.muted = true
-            player.play()
-        } else {
-            let isChapter = false
-            let arr = utils.getUrlParam('nodeId').split('_')
-            let nextId = 0
-            if (arr.length > 2) nextId = arr.pop()
-            let nodeId = arr.join('_')
-            if (utils.isKeepTry()) {
-                utils.notify("找不到视频组件，尝试跳转到下一小节")
-                nodeId += `_${nextId}`
-            } else {
-                GM_deleteValue(utils.getTryKey())
-                isChapter = true
-                utils.notify("找不到视频组件，尝试跳转到下一章")
-            }
-            let nextUrl = utils.getNextUrl(nodeId, isChapter)
-            console.log(nextUrl)
-            setTimeout(() => { window.location.href = nextUrl }, 5000)
-        }
-    }, 5 * 1000);
+    window.onload = function () {
+        utils.controlBox()
+        utils.run()
+    }
 })()
 
 const utils = {
-
-    //生成下一个视频的地址
-    getNextUrl(s, isChapter = false) {
-        let arr = s.split('_')
-        let [prefixStr, chapter] = arr
-        let nodeId = prefixStr + '_'
-        if (arr.length == 2) {
-            nodeId += `${++chapter}`
+    
+    run() {
+        if (utils.currentPageType() == false) {
+            console.log('coursePage')
+            utils.coursePage()
         } else {
-            let [ , , node] = arr
-            node++
-            if (isChapter) {
-                chapter++
+            console.log('playPage')
+            utils.playPage()
+        }
+    },
+
+    // 当前页面类型
+    currentPageType()
+    {
+        let currentUrl = window.location.href
+        //判断是否包含CourseStudy关键词
+        if (currentUrl.indexOf('CourseStudy') > -1) {
+            return false
+        }
+         return true
+    },
+
+    coursePage() {
+
+        // 获取所有章节Id
+        let courseId = utils.getUrlParam('courseId')
+        let nodeIds = [];
+        // 查找ID以courseware开头的A标签
+        let list = document.querySelectorAll('a[id^="courseware"]')
+        for (let i = 0; i < list.length; i++) {
+            //获取href属性
+            let href = list[i].getAttribute('href').replace('javascript: CoursewareNodesManager.onMenuClick(\'', '').replace('\');', '')
+            nodeIds.push(href);
+        }
+
+        if (nodeIds && nodeIds.length <= 0) {
+            window.alert('获取章节失败，请刷新页面重试')
+            return
+        }
+
+        // 写入章节ID
+        GM_setValue('nodeIds_' + courseId, nodeIds.map(v => v.trim()))
+        // 判断是否点击开始学习按钮
+        let start = GM_getValue('start_' + courseId, false)
+        if (utils.isDone()) {
+            utils.notify("所有章节已经学习完毕")
+            return;
+        }
+        if (start == false) return false
+        let nextId = utils.getLastNodeId()
+        window.location.href = window.location.href.replace('CourseStudy', 'CourseShow') + '&nodeId=' + nextId
+
+    },
+
+    playPage() {
+        let courseId = utils.getUrlParam('courseId')
+        let start = GM_getValue('start_' + courseId, false)
+        if (start == false || utils.isDone()) return false
+
+        // 获取所有章节Id
+        let nodeIds = GM_getValue('nodeIds_' + courseId, [])
+        if (nodeIds.length == 0) {
+            window.alert('当前章节数据为空，点击确定开始跳转到课程主页进行获取章节')
+            return
+        }
+
+        // 获取到当前ID
+        let nodeId = utils.getUrlParam('nodeId')
+        let currentNodeId = utils.getLastNodeId();
+        // 获取当前章节ID
+        if (nodeId.trim() != currentNodeId) {
+            window.location.href = window.location.href.replace(nodeId, currentNodeId)
+            return
+        }
+
+        let queryEl = $('video')
+        if (queryEl.get(0) && queryEl.attr('src')) {
+            let vEl = queryEl.get(0)
+            utils.notify("找到视频组件了，准备开始播放视频")
+            vEl.addEventListener("ended", function() {
+                let nodeId = utils.getUrlParam('nodeId')
+                utils.doneNode(nodeId)
+                if (utils.isDone()) {
+                    utils.notify("所有章节已经学习完毕，即将跳转到课程主页")
+                    utils.goCoursePage()
+                } else {
+                    utils.notify("视频播放完毕，即将跳转到下一章节")
+                    utils.goNextNode()
+                }
+            })
+            vEl.muted = true
+            vEl.play()
+        } else {
+            // 没有找到视频组件
+            if (utils.isDone()) {
+                utils.notify("所有章节已经学习完毕，即将跳转到课程主页")
+                utils.goCoursePage()
+            } else {
+                utils.notify("没有找到视频组件，即将跳转到下一章节")
+                window.alert("asdfasd")
+                utils.goNextNode()
             }
-            nodeId += chapter + '_' + node
         }
 
-        let regexp = RegExp(prefixStr + "_\\d{1,2}")
-        if (utils.getUrlParam('nodeId').split('_').length > 2) {
-            regexp = RegExp(prefixStr + "_\\d{1,2}_\\d{1,2}")
-        }
 
-        return window.location.href.replace(regexp, nodeId)
+    },
+
+    getLastNodeId() {
+        let courseId = utils.getUrlParam('courseId')
+        let nodeIds = GM_getValue('nodeIds_' + courseId, [])
+        let doneIds = GM_getValue('done_' + courseId, [])
+        let nodeIdsTodo = nodeIds.filter(v => doneIds.includes(v) == false )
+        return nodeIdsTodo.shift()
+    },
+
+    // 跳转到下一章节
+    goNextNode() {
+        let lastNodeId = utils.getLastNodeId()
+        let currentNodeId = utils.getUrlParam('nodeId')
+        if (nodeIdsTodo && nodeIdsTodo.length > 0 && lastNodeId) {
+            window.location.href = window.location.href.replace(currentNodeId , lastNodeId)
+        }
+    },
+
+    goCoursePage() {
+        window.location.href = window.location.href.replace('CourseStudy', 'CourseShow')
     },
 
     //获取地址栏指定参数
@@ -91,9 +165,64 @@ const utils = {
         }
     },
 
+    isDone()
+    {
+        let courseId = utils.getUrlParam('courseId')
+        let isDone = GM_getValue('isDone_' + courseId, false)
+        if (isDone) return true
+        return false
+    },
+
+    doneNode(nodeId)
+    {
+        let courseId = utils.getUrlParam('courseId')
+        let doneKey = 'done_' + courseId
+        let doneIds = GM_getValue(doneKey, [])
+        if (doneIds.includes(nodeId) == false) {
+            doneIds.push(nodeId)
+            GM_setValue(doneKey, doneIds)
+        }
+        let nodeIds = GM_getValue('nodeIds_' + courseId, [])
+        if (doneIds.length >= nodeIds.length) {
+            GM_setValue('isDone_' + courseId, true)
+        }
+    },
+
+    controlBox()
+    {
+        let courseId = utils.getUrlParam('courseId')
+        let startKey = 'start_' + courseId
+        let start = GM_getValue(startKey, false)
+        let html = `
+            <div style="position: fixed; bottom: 10px; right: 0; z-index: 9999;">
+                <button id="start" style="width: 100px; height: 30px; background:red;  color:#fff; border:none; " id="startStudy">开始自动学习</button>
+            </div>
+        `;
+        if (start) {
+            html = `
+                <div style="position: fixed; bottom: 10px; right: 0; z-index: 9999;">
+                    <button id="stop" style="width: 100px; height: 30px; background:red; color:#fff; border:none; " id="stopStudy">停止自动学习</button>
+                </div>
+            `;
+        }
+
+        $('body').append(html)
+
+        $('#start').click(function () {
+            GM_setValue('start_' + courseId, true)
+            $(this).html('停止自动学习')
+            utils.run()
+        })
+
+        $('#stop').click(function () {
+            GM_setValue('start_' + courseId, false)
+            // 刷新当前页面
+            window.location.reload()
+        })
+    },
+
     //弹出通知
     notify(content) {
-        console.log(content)
         GM_notification({
             title: '系统通知',
             text: content,
@@ -101,27 +230,5 @@ const utils = {
         })
 
     },
-
-    getTryKey() {
-        let planId   = utils.getUrlParam('teachPlanId')
-        let periodId = utils.getUrlParam('periodId')
-        let courseId = utils.getUrlParam('courseId')
-        let nodeId   = utils.getUrlParam('nodeId')
-        let [prefixStr, chapter] = nodeId.split('_')
-        return `${planId}_${periodId}_${courseId}_${prefixStr}_${chapter}`
-    },
-
-    //判断是否继续尝试
-    isKeepTry() {
-        let key = utils.getTryKey()
-        let tryCount = GM_getValue(key, 0)
-        console.log("tryCount = " + tryCount)
-        if (tryCount < 5) {
-            GM_setValue(key, ++tryCount)
-            return true
-        }
-        return false
-    },
-
 
 }
